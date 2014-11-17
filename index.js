2682d8ddfe3b9fea5d22e5ef161ea1f7695b58af
Jerel Unruh <mail@unruhdesigns.com>
2014-11-17 14:54:52 -0600

    Upgraded to ember-cli 1.0.2

--- index.js
+++ index.js
@@ -1,41 +1,10 @@
 'use strict';
 
-var path = require('path');
-var fs   = require('fs');
+module.exports = {
+  name: 'ember-cli-growl',
+  included: function(app) {
+    this._super.included(app);
 
-function EmberCLIGrowl(project) {
-  this.project = project;
-  this.name    = 'Ember CLI Growl';
-}
-
-function unwatchedTree(dir) {
-  return {
-    read:    function() { return dir; },
-    cleanup: function() { }
-  };
-}
-
-EmberCLIGrowl.prototype.treeFor = function treeFor(name) {
-  var treePath = path.relative(process.cwd(), __dirname);
-
-  if (name === 'styles' || name === 'templates') {
-      treePath = path.join(treePath, 'app', name);
-  } else {
-      treePath = path.join(treePath, name);
-  }
-
-  if (fs.existsSync(treePath)) {
-    if (process.env.EMBER_ADDON_ENV === 'development') {
-      return treePath;
-    } else {
-      return unwatchedTree(treePath);
-    }
+    app.import('components/growl-manager.css');
   }
 };
-
-EmberCLIGrowl.prototype.included = function included(app) {
-  this.app = app;
-  this.app.import('components/growl-manager.css');
-};
-
-module.exports = EmberCLIGrowl;
